5c4e853c224ab2f5129ce8f180e76f1b0afbc6dc
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2022-10-29 00:51:07 +0530

    styling

--- client/app.js
+++ client/app.js
@@ -1,6 +1,29 @@
 function App() {
-  return <h1>Hello</h1>
+  //   const [isPressed, setIsPressed] = React.useState(false)
+
+  function handlePressed() {
+    setIsPressed(true)
+  }
+
+  return (
+    <div className="container">
+      <h2>
+        When you press it increments the counter to next state in React's store
+      </h2>
+      <button>Increment</button>
+      <span>
+        <br />
+        <marquee>Counter value</marquee>
+        <br />
+        <h1>9</h1>
+      </span>
+    </div>
+  )
 }
+
+// Print out React
+// console.log(window.React);
+// console.log(ReactDOM);
 
 /************* Render into DOM ****************/
 const rootElement = document.getElementById('root')

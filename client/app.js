2ed3810fa7c8c78b4cc17a113121143f3fe6a6be
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2022-10-29 00:13:25 +0530

    setup to add babel, react, reactdom

---
+++ client/app.js
@@ -0,0 +1,7 @@
+function App() {
+  return <h1>Hello</h1>
+}
+
+/************* Render into DOM ****************/
+const rootElement = document.getElementById('root')
+ReactDOM.render(App(), rootElement)

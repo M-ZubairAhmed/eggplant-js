4672a4cb5f626bc3c3aca3c4d03c867997412422
Md_ZubairAhmed <m-zubairahmed@protonmail.com>
2022-10-29 13:44:45 +0530

    build prod library

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.set('port', port)
 
 
 const clientPath = path.join(__dirname, 'client')
+const libPath = path.join(__dirname, 'lib')
 app.use(static(clientPath))
+app.use(static(libPath))
 
 app.listen(app.get('port'), function () {
   console.log('Server started: http://localhost:' + app.get('port') + '/')
